4343776ca0758d3afe8bb032f19f0219e88fd269
Thanh Lai <thanhlai1993@hotmail.com>
2017-06-29 11:20:06 -0700

    Update csharp.js

--- csharp/csharp.js
+++ csharp/csharp.js
@@ -2,12 +2,21 @@
 
 $("#codeTextarea").bind('paste', function(e) {
     var code = e.originalEvent.clipboardData.getData('text');
+    if (!code) {
+         $('#properties').empty();
+        return;
+    }
      var lines = code.split('\n');
       for(var i = 0;i < lines.length;i++){
+       
+       // get property name
+       var regex = /(.*public\s+)(.*)(\s+{ get; set; }.*)/;
+       var propertyName = lines[i].replace(regex, "$2");
+          
       var property = '<div class="form-check">';
       property += '<label class="form-check-label">';
       property += '<input class="form-check-input" type="checkbox">';
-      property += lines[i];
+      property += propertyName;
       property += '</label>';
       property += '</div>';
       
