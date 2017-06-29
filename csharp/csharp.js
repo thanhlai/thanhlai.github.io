8919683510cef09a1f40457b309a5db882a3d78b
Thanh Lai <thanhlai1993@hotmail.com>
2017-06-29 13:25:37 -0700

    Update csharp.js

--- csharp/csharp.js
+++ csharp/csharp.js
@@ -25,7 +25,9 @@ function extract(text) {
        var propertyName = lines[i].replace(regex, "$2");
           
        if (propertyName.indexOf(" class ") !== -1) {
+           console.log(propertyName);
             model.name = propertyName.trim().match(/class\s(\w*)/, '')[1];
+           console.log(propertyName.trim());
             continue;
        }
           
