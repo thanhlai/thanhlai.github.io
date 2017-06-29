1910d54238781f4733f8cceb5c9b6deed49742a2
Thanh Lai <thanhlai1993@hotmail.com>
2017-06-29 11:25:11 -0700

    Update csharp.js

--- csharp/csharp.js
+++ csharp/csharp.js
@@ -1,5 +1,7 @@
 "use strict"
 
+var letters = /^[0-9a-zA-Z]+$/;
+
 $("#codeTextarea").bind('paste', function(e) {
     var code = e.originalEvent.clipboardData.getData('text');
     if (!code) {
@@ -9,13 +11,20 @@ $("#codeTextarea").bind('paste', function(e) {
      var lines = code.split('\n');
       for(var i = 0;i < lines.length;i++){
        
-       // get property name
+       if (lines.indexOf(" class ") !== -1) {
+            continue;
+       }
+          
        var regex = /(.*public\s+)(.*)(\s+{ get; set; }.*)/;
        var propertyName = lines[i].replace(regex, "$2");
+    
+          if (!letters.test(propertyName)) {
+                continue;
+          }
           
       var property = '<div class="form-check">';
       property += '<label class="form-check-label">';
-      property += '<input class="form-check-input" type="checkbox">';
+      property += '<input class="form-check-input" type="checkbox"> ';
       property += propertyName;
       property += '</label>';
       property += '</div>';
